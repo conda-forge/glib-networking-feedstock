ec6541ae973e09a977eb920176730060839514d3
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-03-20 07:34:57 +0000

    Bump to 2.68.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@ meson setup builddir \
 	--prefix=$PREFIX \
 	--libdir=lib \
 	-Dopenssl=enabled \
-	-Dgnutls=enabled
+	-Dgnutls=disabled
 ninja -v -C builddir -j ${CPU_COUNT}
 ninja -C builddir install -j ${CPU_COUNT}
