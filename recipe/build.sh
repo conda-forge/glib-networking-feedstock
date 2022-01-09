07296e811465866a16ca6f76d660c198c21a0422
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-01-09 19:55:59 +0000

    Fix build issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,9 @@ if [ -z "$MESON_ARGS" ]; then
   MESON_ARGS="--buildtype=release --prefix=${PREFIX} --libdir=lib"
 fi
 
+# _BSD_SOURCE must be set on old versions of Linux to expose some typedefs
+export CPPFLAGS="-D_BSD_SOURCE=1 ${CPPFLAGS}"
+
 meson setup builddir \
 	 ${MESON_ARGS} \
 	-Dopenssl=enabled \
